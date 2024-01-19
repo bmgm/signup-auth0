bd200572998ee775e5d17251346710f5f2a100ec
Mangesh <bmangesh46@gmail.com>
2024-01-19 22:14:53 +0530

    added auth0. Login and Logout button. Also user name when logged in

--- signup-auth0/src/components/navbar/Navbar.jsx
+++ signup-auth0/src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import s from "./Navbar.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
   return (
     <nav>
       <div className={s.wrapper}>
@@ -12,6 +14,27 @@ const Navbar = () => {
             <li>HOME</li>
             <li>ABOUT</li>
             <li>CONTACT</li>
+            {isAuthenticated ? (
+              <div className={s.uname}>
+                <p>{user.name}</p>
+                <li>
+                  <button
+                    onClick={() =>
+                      logout({
+                        logoutParams: { returnTo: window.location.origin },
+                      })
+                    }
+                  >
+                    Log Out
+                  </button>
+                </li>
+              </div>
+            ) : (
+              <div className={s.uname}>
+              <li>
+                <button onClick={() => loginWithRedirect()}>Log In</button>
+              </li></div>
+            )}
           </ul>
         </div>
       </div>
